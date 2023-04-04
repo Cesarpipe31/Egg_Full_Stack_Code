9ae002ccb26247e54aa6f66fa3280416be519a67
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-04-04 12:12:18 -0500

    publicando

---
+++ Z002_JavInt_001_primer_Programa/src/z002_javint_001_primer_programa/Z002_JavInt_001_primer_Programa.java
@@ -0,0 +1,10 @@
+
+package z002_javint_001_primer_programa;
+
+public class Z002_JavInt_001_primer_Programa {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
